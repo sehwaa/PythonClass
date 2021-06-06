3e19ae38058fd497327ed65d0b326558c7c68ae4
cocoa1021 <13cocoa1021@gmail.com>
2021-06-06 23:18:04 +0900

    [210606] 스터디 보충 과제

---
+++ 210606_mp3.py
@@ -0,0 +1,22 @@
+class Mp3:
+	def __init__(self, song1, song2):
+		self.song1 = song1
+		self.song2 = song2
+		
+	def add_song(self):
+		print(self.song + )
+		
+	def add_song_play(self):
+		print(self.song + "재생")
+		
+	def add_song_pause(self):
+		print(self.song + "일시정지")
+		
+	def add_song_delete(self):
+		pring(self.song + "삭제")
+		
+
+BTS = Mp3("노래1", "노래2")
+BTS.add_song_play()
+BTS.add_song_pause()
+BTS.add_song_delete()
