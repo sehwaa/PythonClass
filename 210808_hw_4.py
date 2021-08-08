03864ac8aa4b6ace804ba3662b7e9adc97e02303
cocoa1021 <13cocoa1021@gmail.com>
2021-08-08 18:44:19 +0900

    [210808] Chapter 6 연습문제 수정

--- 210808_hw_4.py
+++ 210808_hw_4.py
@@ -5,3 +5,7 @@ c = 12
 print(a + "는 string 문자열")
 print(str(b) + "는 float 실수형")
 print(str(c) + "는 int 정수형")
+
+print(type(a))
+print(type(b))
+print(type(c))
--- 210808_hw_8.py
+++ 210808_hw_8.py
@@ -4,3 +4,4 @@ print(int(year) - 4)
 print(int(year) + 4)
 
 print(str(int(year) - 4) + "\n" + str(int(year) + 4))
+#print를 하나만 쓰고 줄바꿈을 하고 싶었어
