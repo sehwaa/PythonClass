b4a981fa16eb049c96dc4875a3a65a3b92f8d2d4
Jiwon Kim <13cocoa1021@gmail.com>
2021-10-04 21:59:06 +0900

    [210919] Chapter 10 과제 리뷰

--- 210919_jiwon/210919_hw_1.py
+++ 210919_jiwon/210919_hw_1.py
@@ -2,3 +2,8 @@ hi = ["NICE", "TO", "MEET", "YOU"]
 
 for h in hi:
     print(h.lower())
+# hi = ["NICE", "TO", "MEET", "YOU"]
+
+# for idx in range (0, len(hi)): # 0, 1, 2, 3 > len는 hi의 길이. 4니까 4미만이므로 3까지
+#     hi[idx] = hi[idx].lower()
+# print(hi)
