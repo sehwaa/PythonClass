525edd2946230e25e13a813a2d6ff43296cae5f4
cocoa1021 <84976079+cocoa1021@users.noreply.github.com>
2021-10-06 01:24:07 +0900

    Jiwon (#7)

---
+++ 210919_jiwon/210919_hw_2.py
@@ -0,0 +1,4 @@
+sale = [12400, 34500, 34560, 134520]
+
+for s in sale:
+    print(int(s*1.1))
---
+++ 210919_jiwon/210919_hw_7.py
@@ -0,0 +1,9 @@
+list = [ [1, 2], [3, 4], [5, 6] ]
+
+for i in list:
+    print(i)
+# list = [ [1, 2], [3, 4], [5, 6] ]
+
+# for i in list:
+#     for j in i:
+#         print(j)
